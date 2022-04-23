5e35429a8d2135f1f24316702ad5456722fff7a5
Misha2048 <mikaelan.itsmart@gmail.com>
2022-04-23 15:09:52 +0300

    templates basics + css normailizer

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -118,6 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = 'mediafiles/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
